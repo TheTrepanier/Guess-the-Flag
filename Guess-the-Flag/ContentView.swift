e12d9cf4577bfc76a458b007234af5d808e7f533
TheTrepanier_Dev <TheTrepanier@icloud.com>
2021-02-01 19:19:34 -0700

    basic app completed, challenges completed as well, updated names of UK and US to show as The UK and The US with changes to assests included

--- Guess-the-Flag/ContentView.swift
+++ Guess-the-Flag/ContentView.swift
@@ -1,6 +1,6 @@
 //
 //  ContentView.swift
-//  Guess-the-Flag
+//  Guess the Flag - Hacking with Swift
 //
 //  Created by Michael Trepanier on 1/31/21.
 //
@@ -8,9 +8,67 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "The UK", "The US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    @State private var alertMessage = ""
+    @State private var score = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
+            
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of")
+                        .foregroundColor(.white)
+                    Text(countries[correctAnswer])
+                        .foregroundColor(.white)
+                        .font(.largeTitle)
+                        .fontWeight(.black)
+                }
+                ForEach(0 ..< 3) { number in
+                    Button(action: {
+                        self.flagTapped(number)
+                    }) {
+                        Image(self.countries[number])
+                            .renderingMode(.original)
+                            .clipShape(Capsule())
+                            .overlay(Capsule().stroke(Color.black, lineWidth: 1))
+                            .shadow(color: .black, radius: 2)
+                    }
+                }
+                Spacer()
+                Text("Current Score: \(score)")
+                    .foregroundColor(.white)
+                    .font(.headline)
+            }
+        }
+        .alert(isPresented: $showingScore) {
+            Alert(title: Text(scoreTitle), message: Text(alertMessage), dismissButton: .default(Text("Continue")) {
+                self.askQuestion()
+            })
+        }
+            
+    }
+    
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+            score += 1
+            alertMessage = "Your score is \(score)"
+        } else {
+            scoreTitle = "Apologies!"
+            alertMessage = "That's the flag of \(self.countries[number])"
+        }
+        
+        showingScore = true
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
     }
 }
 
---
+++ README.md
@@ -0,0 +1,11 @@
+## Guess The Flag
+
+[Paul Hudson](https://www.hackingwithswift.com/about) * [@twostraws](https://twitter.com/twostraws)
+
+### Intro
+A simple SwiftUI project that uses stacks, buttons, alerts, and more.
+
+### Challenge
+- [x] Add an `@State` property to store the user’s score, modify it when they get an answer right or wrong, then display it in the alert.
+- [x] Show the player’s current score in a label directly below the flags.
+- [x] When someone chooses the wrong flag, tell them their mistake in your alert message – something like “Wrong! That’s the flag of France,” for example.
